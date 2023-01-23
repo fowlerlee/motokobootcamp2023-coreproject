923b620e452f172bfa61068a44624dd8fa7c0b4c
fleeB1 <fowler.lee8@gmail.com>
2023-01-24 00:39:26 +0100

    launch local and remote leder

---
+++ ledger_local.sh
@@ -0,0 +1,11 @@
+dfx identity use default
+export LEDGER_ACC=$(dfx ledger account-id)
+
+dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}})'
+
+dfx identity use default
+export ARCHIVE_CONTROLLER=$(dfx identity get-principal)
+
+dfx deploy ledger --argument '(record {minting_account = "'${MINT_ACC}'"; initial_values = vec { record { "'${LEDGER_ACC}'"; record { e8s=100_000_000_000 } }; }; send_whitelist = vec {}; archive_options = opt record { trigger_threshold = 2000; num_blocks_to_archive = 1000; controller_id = principal "'${ARCHIVE_CONTROLLER}'" }})'
+
+dfx canister call ledger account_balance '(record { account = '$(python3 -c 'print("vec{" + ";".join([str(b) for b in bytes.fromhex("'$LEDGER_ACC'")]) + "}")')' })'
