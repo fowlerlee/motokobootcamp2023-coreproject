baa7ca61deaec2550911283a4e6bfebfbf1376b8
fleeB1 <fowler.lee8@gmail.com>
2023-02-09 22:35:03 +0100

    impl transfer

--- src/rustycan/src/service.rs
+++ src/rustycan/src/service.rs
@@ -1,5 +1,7 @@
 
 use std::collections::{BTreeMap, BTreeSet};
+use candid::Principal;
+use crate::STORE;
 use crate::types::Store;
 
 impl Default for Store {
@@ -9,5 +11,15 @@ impl Default for Store {
             users: BTreeSet::new(),
             wallets: BTreeMap::new(),
         }
+    }
+}
+
+impl Store {
+
+    fn transfer(to: Principal, from: Principal) -> Result<(),()> {
+        let to_account = STORE.with(|s|{s.borrow().wallets.get(&to)});
+        let from_account = STORE.with(|s| {s.borrow().wallets.get(&from)});
+        Ok(())
+
     }
 }
